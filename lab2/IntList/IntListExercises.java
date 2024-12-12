1efa606ece23a625a09a2a3d17bfd9b213186f77
forsurfing <justforsurfingtheinternet@gmail.com>
2024-12-12 14:35:20 +0800

    Update IntListExercises.java

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -80,6 +80,6 @@ public class IntListExercises {
             lst.first *= lst.first;
         }
 
-        return currElemIsPrime || squarePrimes(lst.rest);
+        return currElemIsPrime | squarePrimes(lst.rest);
     }
 }
