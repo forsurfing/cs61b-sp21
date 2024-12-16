6bb5f3636d86f20d0c4baf97b1dfd699a499b242
forsurfing <justforsurfingtheinternet@gmail.com>
2024-12-16 11:06:59 +0800

    Update TimeSLList.java

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -22,7 +22,25 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        int[] testSizes = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+        for(int N : testSizes) {
+            AList<Integer> test = new AList<>();
+            for(int i=0;i<N;i++) {
+                test.addLast(i);
+            }
+            Stopwatch sw = new Stopwatch();
+            for(int i = 0; i < N; i++) {
+                test.getLast();
+            }
+            double timeInSeconds = sw.elapsedTime();
+            Ns.addLast(N);
+            opCounts.addLast(N);
+            times.addLast(timeInSeconds);
+       }
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
