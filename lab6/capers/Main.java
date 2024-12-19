6481022a5d33a21baa36802818fa09f831e9046f
forsurfing <justforsurfingtheinternet@gmail.com>
2024-12-19 09:50:07 +0800

    Update Main.java

--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -1,8 +1,9 @@
 package capers;
 
 import java.io.File;
-
+import java.util.Arrays;
 import static capers.Utils.*;
+
 
 /** Canine Capers: A Gitlet Prelude.
  * @author TODO
@@ -40,7 +41,6 @@ public class Main {
         if (args.length == 0) {
             Utils.exitWithError("Must have at least one argument");
         }
-
         CapersRepository.setupPersistence();
         String text;
         switch (args[0]) {
@@ -52,10 +52,12 @@ public class Main {
             break;
         case "dog":
             validateNumArgs("dog", args, 4);
+            CapersRepository.makeDog(args[1],args[2], Integer.parseInt(args[3]));
             // TODO: make a dog
             break;
         case "birthday":
             validateNumArgs("birthday", args, 2);
+            CapersRepository.celebrateBirthday(args[1]);
             // TODO: celebrate this dog's birthday
             break;
         default:
